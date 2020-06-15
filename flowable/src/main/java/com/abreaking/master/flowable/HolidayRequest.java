f25569c115f360f12ec2e47f18e77c15a9778907
aBreaking <abreaking@163.com>
2020-06-15 17:52:52 +0800

    flowable项目初始化，创建基本表

---
+++ flowable/src/main/java/com/abreaking/master/flowable/HolidayRequest.java
@@ -0,0 +1,30 @@
+package com.abreaking.master.flowable;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+
+/**
+ * the first project of flowable
+ * @author liwei_paas
+ * @date 2020/6/15
+ */
+public class HolidayRequest {
+
+    static String JDBC_URl = "jdbc:mysql://localhost:3306/flowable";
+    static String JDBC_USERNAME = "root";
+    static String JDBC_PASSWORD = "mysqladmin";
+    static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+    public static void main(String args[]){
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl(JDBC_URl)
+                .setJdbcUsername(JDBC_USERNAME)
+                .setJdbcPassword(JDBC_PASSWORD)
+                .setJdbcDriver(JDBC_DRIVER)
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+    }
+
+}
