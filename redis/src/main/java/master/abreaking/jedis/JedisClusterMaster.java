d39b8d30714a016159285bf1989f2fa12b9b8b37
aBreaking <abreaking@163.com>
2020-07-30 15:10:37 +0800

    master of redis。jedisCluter连接数测试

---
+++ redis/src/main/java/master/abreaking/jedis/JedisClusterMaster.java
@@ -0,0 +1,86 @@
+package master.abreaking.jedis;
+
+import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * JedisCluster使用
+ * @author liwei_paas
+ * @date 2020/7/28
+ */
+public class JedisClusterMaster {
+
+    final static String redis_ip_port = "127.0.0.1:7001,127.0.0.1:7002,127.0.0.1:7003,127.0.0.1:7004,127.0.0.1:7005,127.0.0.1:7006";
+    final static String redis_p = "5172551";
+    final static Set<HostAndPort> clusterNodes = new HashSet<HostAndPort>();
+    static {
+        List<String> addresses = Arrays.asList(redis_ip_port.split(","));
+        for (String addr : addresses) {
+            String[] s = addr.split(":");
+            clusterNodes.add(new HostAndPort(s[0],Integer.parseInt(s[1])));
+        }
+    }
+
+    @Test
+    public void jedisCluster() throws InterruptedException, IOException {
+        JedisCluster cluster = getJedisCluster();
+        String name = cluster.get("name");
+        System.out.println(name);
+        // cluster.close();
+    }
+
+    @Test
+    public void anyJedisConnect() throws Exception {
+        for (int i = 0; i < 100; i++) {
+            //connected_clients:101
+            new Thread(()->{
+                final JedisCluster cluster = getJedisCluster();
+                String name = cluster.get("name");
+                System.out.println(name);
+            }).start();
+        }
+        System.out.println("test connect number");
+        Thread.sleep(10000);
+    }
+    @Test
+    public void jedisConnect() throws Exception {
+        //一个cluster默认 8个连接
+        final JedisCluster cluster = getJedisCluster();
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->{
+                String name = cluster.get("name");
+                System.out.println(name);
+            }).start();
+        }
+        final JedisCluster cluster2 = getJedisCluster();
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->{
+                String name = cluster2.get("name");
+                System.out.println(name);
+            }).start();
+        }
+        final JedisCluster cluster3 = getJedisCluster();
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->{
+                String name = cluster3.get("name");
+                System.out.println(name);
+            }).start();
+        }
+
+        System.out.println("test connect number");
+        Thread.sleep(10000);
+    }
+
+    private static JedisCluster getJedisCluster(){
+        JedisCluster cluster = new JedisCluster(clusterNodes, 3000, 3000, 1000, redis_p, new JedisPoolConfig());
+        return cluster;
+    }
+}
---
+++ redis/src/main/java/master/abreaking/jedis/SpringRedisMaster.java
@@ -0,0 +1,35 @@
+package master.abreaking.jedis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.jedis.JedisCluster;
+
+import javax.annotation.Resource;
+
+/**
+ * spring-redis rest
+ * @author liwei_paas
+ * @date 2020/7/30
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-config.xml")
+public class SpringRedisMaster {
+
+    @Resource
+    JedisCluster jedisCluster;
+
+    @Test
+    public void test01() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->{
+
+                String name = jedisCluster.hget("routeInfo","10_183");
+                System.out.println(name);
+            }).start();
+        }
+        System.out.println("test connect number");
+        Thread.sleep(10000);
+    }
+}
