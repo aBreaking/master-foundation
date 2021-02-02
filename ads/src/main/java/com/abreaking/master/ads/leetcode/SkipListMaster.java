66eb86cc0a7eb259155010f88575d77528f94b69
aBreaking <abreaking@163.com>
2021-02-02 11:13:41 +0800

    跳表实现优化

--- ads/src/main/java/com/abreaking/master/ads/leetcode/SkipListMaster.java
+++ ads/src/main/java/com/abreaking/master/ads/leetcode/SkipListMaster.java
@@ -1,6 +1,8 @@
 package com.abreaking.master.ads.leetcode;
 
 
+import java.util.*;
+
 /**
  * 跳表的设计
  * from : https://leetcode-cn.com/problems/design-skiplist
@@ -26,21 +28,34 @@ public class SkipListMaster {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-            double random = Math.random();
-            System.out.println(random);
-        }
+        int count = 100 * 1024;
 
-        if (true){
-            return;
-        }
-
+        long l1 = System.currentTimeMillis();
         Skiplist skiplist = new Skiplist();
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < count; i++) {
             skiplist.add(i);
         }
+        long l2 = System.currentTimeMillis();
+        System.out.println("Skiplist add ->"+(l2-l1));
 
-        System.out.println(skiplist);
+        Set set = new TreeSet();
+        for (int i = 0; i < count; i++) {
+            set.add(i);
+        }
+        long l3 = System.currentTimeMillis();
+        System.out.println("TreeSet add ->" +(l3-l2) );
+
+        System.out.println(skiplist.search(99));
+        System.out.println(skiplist.search(100531));
+        System.out.println(skiplist.search(367324324));
+
+        long l4 = System.currentTimeMillis();
+        System.out.println("skip list find ->" + (l4-l3));
+        System.out.println(set.contains(99));
+        System.out.println(set.contains(100531));
+        System.out.println(set.contains(367324324));
+        long l5 = System.currentTimeMillis();
+        System.out.println("set find ->" + (l5-l4));
     }
 
     static class Skiplist {
@@ -53,7 +68,7 @@ public class SkipListMaster {
          */
         private static double DEFAULT_P_FACTOR = 0.25;
 
-        Node head = new Node(null); //头节点
+        Node head = new Node(null,DEFAULT_MAX_LEVEL); //头节点
 
         int currentLevel = 1; //表示当前nodes的实际层数，它从1开始
 
@@ -77,25 +92,30 @@ public class SkipListMaster {
          * @param num
          */
         public void add(int num) {
-            int randomLevel = randomLevel();
-            Node newNode = new Node(num);
-
+            int level = randomLevel();
             Node updateNode = head;
+            Node newNode = new Node(num,level);
             // 计算出当前num 索引的实际层数，从该层开始添加索引
-            int level = randomLevel > currentLevel ? currentLevel:randomLevel;
-            for (int i = level-1; i>=0; i--) {
-                //找到本层最近离num最近的list
+            for (int i = currentLevel-1; i>=0; i--) {
+                //找到本层最近离num最近的节点，而后开始设置每一层该num的跳表
                 updateNode = findClosest(updateNode,i,num);
-                Node temp = updateNode.next[i];
-                updateNode.next[i] = newNode;
-                newNode.next[i] = temp;
+                if (i<level){
+                    if (updateNode.next[i]==null){
+                        updateNode.next[i] = newNode;
+                    }else{
+                        Node temp = updateNode.next[i];
+                        updateNode.next[i] = newNode;
+                        newNode.next[i] = temp;
+                    }
+                }
             }
-            if (randomLevel > currentLevel){ //如果随机出来的层数比当前的层数还大，那么超过currentLevel的head 直接指向newNode
-                for (int i = currentLevel; i < randomLevel; i++) {
+            if (level > currentLevel){ //如果随机出来的层数比当前的层数还大，那么超过currentLevel的head 直接指向newNode
+                for (int i = currentLevel; i < level; i++) {
                     head.next[i] = newNode;
                 }
-                currentLevel = randomLevel;
+                currentLevel = level;
             }
+
         }
 
         public boolean erase(int num) {
@@ -121,12 +141,8 @@ public class SkipListMaster {
          * @return
          */
         private Node findClosest(Node node,int levelIndex,int value){
-            Node n;
-            while ((n=node.next[levelIndex])!=null){
-                if (value<=n.value){
-                    break;
-                }
-                node = n;
+            while ((node.next[levelIndex])!=null && value >node.next[levelIndex].value){
+                node = node.next[levelIndex];
             }
             return node;
         }
@@ -148,9 +164,9 @@ public class SkipListMaster {
             Integer value;
             Node[] next;
 
-            public Node(Integer value) {
+            public Node(Integer value,int size) {
                 this.value = value;
-                this.next = new Node[DEFAULT_MAX_LEVEL];
+                this.next = new Node[size];
             }
 
             @Override
